90d60757e30f5948e375e1c4f22169cfbefa5ee1
Cavan Theiss <cavanrt@gmail.com>
2026-04-15 15:13:49 -0500

    Update ShortestDistancetoTargetStringinaCircularArray.cpp

--- ShortestDistancetoTargetStringinaCircularArray.cpp
+++ ShortestDistancetoTargetStringinaCircularArray.cpp
@@ -2,11 +2,16 @@
 Rank: Easy
 Title: Shortest Distance to Target String in a Circular Array
 Author: Cavan Ray Theiss
+
 Description: Given a circular array of strings, a starting point, and 
 a word to look for, We need to return the shortest distance
 from our starting point to the word we need to search for.
+
 Input: An array of strings, a string to search for, an integer
 for the starting index where we begin
+
+Output: an integer representing the smallest distance between the starting
+index and the string we are searching for
 */
 
 class Solution {
