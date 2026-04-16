ea91a7f4a5aefccb778b0e5a7429cb2119bee744
Cavan Theiss <cavanrt@gmail.com>
2026-04-16 15:49:13 -0500

    Update DiffuseTheBomb.cpp

--- DiffuseTheBomb.cpp
+++ DiffuseTheBomb.cpp
@@ -4,6 +4,12 @@ Author: Cavan Ray Theiss
 
 Description: decrypt the code to diffuse the bomb
 
+If k > 0, replace the ith number with the sum of the next k numbers.
+If k < 0, replace the ith number with the sum of the previous -k numbers.
+If k == 0, replace the ith number with 0.
+
+The array is circular, so when you reach the end it goes back to the beginning
+
 */
 
 class Solution {
