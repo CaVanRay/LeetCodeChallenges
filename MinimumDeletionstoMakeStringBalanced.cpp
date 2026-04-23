283021f814984fd189a4bdcee2b1797668f92634
Cavan Theiss <cavanrt@gmail.com>
2026-04-23 08:40:57 -0500

    Update MinimumDeletionstoMakeStringBalanced.cpp

--- MinimumDeletionstoMakeStringBalanced.cpp
+++ MinimumDeletionstoMakeStringBalanced.cpp
@@ -10,6 +10,10 @@ You are given a string s consisting only of characters 'a' and 'b'​​​​.
 You can delete any number of characters in s to make s balanced. 
 s is balanced if there is no pair of indices (i,j) such that i < j and s[i] = 'b' and s[j]= 'a'.
 
+to clarify:
+You must delete characters so no 'b' appears before any 'a'. 
+The goal is to maximize the number of characters kept, specifically by keeping a valid split point between 'a's and 'b's. 
+
 Return the minimum number of deletions needed to make s balanced.
 */
 
