bb8b674e6f51b7d3580c1f9b28684025081101ed
Cavan Theiss <cavanrt@gmail.com>
2026-05-02 23:30:34 -0500

    Update MinimumJumpsToReachHome.cpp

--- MinimumJumpsToReachHome.cpp
+++ MinimumJumpsToReachHome.cpp
@@ -113,7 +113,7 @@ public:
               (visitedSpots.count(currentSpot - backwardJump) == 0 || !(visitedSpots[currentSpot - backwardJump].visitedByBackwJump))){ // if either not visited, or not visited by a backwards jump
         
                 nextToVisit.push( nextToVisit.front() + (-backwardJump));
-                if(visitedSpots.count(currentSpot) == 0){
+                if(visitedSpots.count(currentSpot -  backwardJump) == 0){
                         visitedSpots[currentSpot] = nextToVisit.front();
                 }else{
                 visitedSpots[currentSpot].visitedByBackwJump = nextToVisit.front().justJumpedBack;
