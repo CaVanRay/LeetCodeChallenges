9d5cda36c5a34d49a546df4f6a73c0e3fc13ad01
Cavan Theiss <cavanrt@gmail.com>
2026-05-04 16:49:54 -0500

    Update 1655.cpp

--- 1655.cpp
+++ 1655.cpp
@@ -22,24 +22,13 @@ public:
     bool canDistribute(vector<int>& nums, vector<int>& quantity) {
         unordered_map<int, int> dupesFound;
 
-        /**********************************************************
-        * Ok, here's my idea, we count duplicates and take orders *
-        * at the same time.                                       *
-        *                                                         *
-        * as we go through and check for duplicates of each num   *
-        * we keep the order list up and everytime the current     *
-        * order count is filled it gets rolled over to the next   *
-        * order on the list                                       *
-        *                                                         *   
-        * if we run out of orders before num's, we return true,   *
-        * if we run out of num's before orders, we return false   *
-        *                                                         *       
-        * my only worry is that this solution seems to easy for   *
-        * a challenge ranked 'hard', especially seeing how the    *
-        * medium level challenge I just finished took about a     *
-        * week to finish                                          *
-        **********************************************************/
-
+        /************************
+        * Just make it dirty    *
+        *                       *
+        * we can optimize once  *
+        * it's working          *
+        ************************/
+        
         if(quantity.size() > nums.size()){ // if we have more requests then items, it's not possible to serve everyone
             return false;
         }
