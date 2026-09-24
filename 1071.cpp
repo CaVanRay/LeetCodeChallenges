d714861d75d43a64d9066bc7eeded65ed3981a5d
Cavan Theiss <cavanrt@gmail.com>
2026-09-24 14:49:40 -0500

    Create 1071.cpp
    21

---
+++ 1071.cpp
@@ -0,0 +1,12 @@
+/*
+Challenge #: 1071
+Difficulty: Easy
+Title: Greatest Common Divisor of Strings
+Author: Cavan Ray Theiss
+
+Description:
+
+For two strings s and t, we say "t divides s" if and only if s = t + t + t + ... + t + t (i.e., t is concatenated with itself one or more times).
+
+Given two strings str1 and str2, return the largest string x such that x divides both str1 and str2.
+*/
