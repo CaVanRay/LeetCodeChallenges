e358f36042df99809842f534d72b504e98935b51
Cavan Theiss <cavanrt@gmail.com>
2026-09-24 14:36:17 -0500

    Update 1768.cpp
    17

--- 1768.cpp
+++ 1768.cpp
@@ -1,1 +1,15 @@
+/*
+Challenge #: 1768
+Difficulty: Easy
+Title: Merge Strings Alternately
+Author: Cavan Ray Theiss
 
+Description:
+
+You are given two strings word1 and word2. 
+Merge the strings by adding letters in alternating order, starting with word1. 
+If a string is longer than the other, append the additional letters onto the end of the merged string.
+
+Return the merged string.
+
+*/
