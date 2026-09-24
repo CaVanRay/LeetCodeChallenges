8353b1f11218bc8aca524860efcf71dbc387cc75
Cavan Theiss <cavanrt@gmail.com>
2026-09-24 14:44:06 -0500

    Update 18 to 1768.cpp
    18

--- 1768.cpp
+++ 1768.cpp
@@ -13,3 +13,22 @@ If a string is longer than the other, append the additional letters onto the end
 Return the merged string.
 
 */
+
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+      string word3;
+      int k = 0;
+      for(char letter : word1){
+        word3.push_back(letter);
+        if(k <= word2.size()){
+          word3.push_back(word2[k]);
+        }
+        k++;
+      }
+      while(k <= word2.size()){
+        word3.push_back(word2[k]);
+        k++
+      } 
+    }
+};
